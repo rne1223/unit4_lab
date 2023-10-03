2c77a19edf441db5d45aebfca272534156ab6248
rne1223 <rne1223@gmail.com>
2023-10-02 23:16:11 -0700

    Final

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,179 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import APIForm from './components/APIForm'; 
+import Gallery from './components/Gallery';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const ACCESS_KEY = '0e6a845f162f4a9e80e34f80c5f4d428'
+
+  const [inputs, setInputs] = useState({
+    url: "",
+    format: "",
+    no_ads: "",
+    no_cookie_banners: "",
+    width: "",
+    height: "",
+  });
+
+  const [currentImage, setCurrentImage] = useState(null);
+
+  const callAPI = async (query) => {
+
+    const response = await fetch(query);
+    const json = await response.json();
+
+    if (json.url == null) {
+      alert("Oops! Something went wrong with that query, let's try again!")
+    }
+    else {
+      setCurrentImage(json.url);
+      setPrevImages((images) => [...images, json.url]);
+      reset();
+      getQuota();
+    }
+  }
+
+  const makeQuery = () => {
+
+    let wait_until = "network_idle";
+    let response_type = "json";
+    let fail_on_status = "400%2C404%2C500-511";
+    let url_starter = "https://";
+    let fullURL = url_starter + inputs.url;
+
+    let query = `https://api.apiflash.com/v1/urltoimage?
+    access_key=${ACCESS_KEY}&
+    url=${fullURL}&
+    format=${inputs.format}&
+    width=${inputs.width}&
+    height=${inputs.height}&
+    no_cookie_banners=${inputs.no_cookie_banners}&
+    no_ads=${inputs.no_ads}&
+    wait_until=${wait_until}&
+    response_type=${response_type}&
+    fail_on_status=${fail_on_status}`;
+
+    callAPI(query).catch(console.error);
+  }
+
+  const submitForm = () => {
+    let defaultValues = {
+      format: "jpeg",
+      no_ads: "true",
+      no_cookie_banners: "true",
+      width: "1920",
+      height: "1080",
+    };
+
+    if (inputs.url == "" || inputs.url == " ") {
+      alert("You forgot to submit an url!");
+    }
+    else {
+      for (const [key, value] of Object.entries(inputs)) {
+        if (value == "") {
+          inputs[key] = defaultValues[key]
+        }
+      }
+      makeQuery()
+    }
+  }
+
+
+  const [quota, setQuota] = useState(null);
+  const getQuota = async () => {
+    const response = await fetch(`https://api.apiflash.com/v1/urltoimage/quota?access_key=${ACCESS_KEY}`);
+    const result = await response.json();
+
+    setQuota(result);
+  }
+
+
+  const reset = () => {
+    setInputs({
+      url: "",
+      format: "",
+      no_ads: "",
+      no_cookie_banners: "",
+      width: "",
+      height: "",
+    });
+  }
+  const [prevImages, setPrevImages] = useState([]);
+
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+    <div className="whole-page">
+      <h1>Build Your Own Screenshot! 📸</h1>
+
+      <APIForm
+        inputs={inputs}
+        handleChange={(e) =>
+          setInputs((prevState) => ({
+            ...prevState,
+            [e.target.name]: e.target.value.trim(),
+          }))
+        }
+        onSubmit={submitForm}
+      />
+      <br></br>
+
+      {currentImage ? (
+        <img
+          className="screenshot"
+          src={currentImage}
+          alt="Screenshot returned"
+        />
+      ) : (
+        <div> </div>
+      )}
+
+      <div className="container">
+        <h3> Current Query Status: </h3>
+
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          https://api.apiflash.com/v1/urltoimage?access_key=ACCESS_KEY
+          <br></br>
+          &url={inputs.url} <br></br>
+          &format={inputs.format} <br></br>
+          &width={inputs.width}
+          <br></br>
+          &height={inputs.height}
+          <br></br>
+          &no_cookie_banners={inputs.no_cookie_banners}
+          <br></br>
+          &no_ads={inputs.no_ads}
+          <br></br>
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+
+
+
+      <br></br>
+
+
+
+
+
+      <div className="container">
+        <Gallery images={prevImages} />
+      </div>
+
+      {quota ? (
+        <p className="quota">
+          {" "}
+          Remaining API calls: {quota.remaining} out of {quota.limit}
+        </p>
+      ) : (
+        <p></p>
+      )}
+
+
+
+
+    </div>
+  );
 }
+
 
 export default App
